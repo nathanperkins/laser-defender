eaeea2fd406f9a22876d827dbac041bb95b0af81
Nathan Perkins <nperkins487@gmail.com>
2021-07-18 15:49:45 -0700

    Clamp player movement based on camera

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10f;
+    float xMin, xMax, yMin, yMax;
+
+    void Start()
+    {
+        SetMoveBoundaries();
+    }
 
     // Update is called once per frame
     void Update()
@@ -12,14 +16,26 @@ public class Player : MonoBehaviour
         Move();    
     }
 
+    private void SetMoveBoundaries()
+    {
+        Camera cam = Camera.main;
+        float halfOfShipX = transform.localScale.x / 2;
+        float halfOfShipY = transform.localScale.y / 2;
+
+        xMin = cam.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + halfOfShipX;
+        xMax = cam.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - halfOfShipX;
+        yMin = cam.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + halfOfShipY;
+        yMax = cam.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - halfOfShipY;
+	}
+
     private void Move()
     {
         float deltaX = ScaleMovement(Input.GetAxis("Horizontal"));
         float deltaY = ScaleMovement(Input.GetAxis("Vertical"));
 
         Vector3 pos = transform.position;
-        pos.x += deltaX;
-        pos.y += deltaY;
+        pos.x = Mathf.Clamp(pos.x + deltaX, xMin, xMax);
+        pos.y = Mathf.Clamp(pos.y + deltaY, yMin, yMax);
         transform.position = pos;
     }
 
