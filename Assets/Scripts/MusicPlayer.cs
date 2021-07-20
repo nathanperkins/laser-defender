684c641c4268cdb6ff3cdb68fb50e9c78a2388a5
Nathan Perkins <nperkins487@gmail.com>
2021-07-20 00:10:42 -0700

    Add MusicPlayer

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    void Awake()
+    {
+        SetupSingleton();
+    }
+
+    private void SetupSingleton()
+    {
+        int musicPlayerCount = FindObjectsOfType(GetType()).Length;
+        if (musicPlayerCount > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
